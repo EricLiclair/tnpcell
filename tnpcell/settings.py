509434443d736fe084b416de1aa9d254665c3c1d
Shubham Kushwaha <eric.liclair@gmail.com>
2021-03-09 17:04:38 +0530

    hiding the secret key

--- tnpcell/settings.py
+++ tnpcell/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from secret_settings import _SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#u@s1tu*$&my)zn=bm+!r$al^@-k8v2n31n6qmwa2=!gh3%6y2'
+SECRET_KEY = _SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
