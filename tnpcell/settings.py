08117ddb796b4c00a06a1862897296b9fee8fb89
Shubham Kushwaha <eric.liclair@gmail.com>
2021-03-09 17:19:02 +0530

    adding the secret key through config variable

--- tnpcell/settings.py
+++ tnpcell/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from .secret_settings import _SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = _SECRET_KEY
+SECRET_KEY = os.getenv('SECRET_KEY', 'some28934y(*&*#&(^$*@ajdhfiauhf87878*^Gda')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
